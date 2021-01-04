3690ef18c2de10e54db339ff42b27a33c1aded6b
MHD22 <mhd.sad22@gmail.com>
2021-01-05 00:09:36 +0200

    clean up the code, remove welcome function

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -3,7 +3,6 @@ public class test {
     public static void main(String[] args) {
 
         System.out.println("hello git");
-
         System.out.println("hello 3");
 
 
@@ -11,9 +10,7 @@ public class test {
 
 
     }
-    public static void printWelcome(){
-        System.out.println("Welcome to my demo");
-    }
+
     public static void printBye(){
         System.out.println("Bye Bye.. have a good day.");
     }
