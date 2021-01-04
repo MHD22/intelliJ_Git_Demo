ad2a099dbc46875dd30362b6ea4be6212d4162a3
MHD22 <mhd.sad22@gmail.com>
2021-01-05 00:05:05 +0200

    call the functions ( welcome & Bye )

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,9 +2,8 @@
 public class test {
     public static void main(String[] args) {
 
-        System.out.println("hello git");
-
-        System.out.println("hello 3");
+        printWelcome();
+        printBye();
 
 
 
